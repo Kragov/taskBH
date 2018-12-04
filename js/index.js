23510c86f5546c0bc4d95f6915ca9474da713659
Kragov <34142271+Kragov@users.noreply.github.com>
2018-12-04 21:25:08 +0300

    Add files via upload
    It's aliiiiiiivvveeeee !!!

--- js/index.js
+++ js/index.js
@@ -2,14 +2,34 @@
 
 $(document).ready(function(){
 
-    $("#sliderContainer > .slide:gt(0)").hide();
+    $("#sliderContainer > .slide:gt(0)").hide()
+    
+mrgn = -800;
 
-    setInterval(function () {
-        $("#sliderContainer > .slide:first")
-            .fadeOut(1000)
-            .next()
-            .fadeIn(1000)
-            .end()
-            .appendTo('#slideshow');
-    }, 3000)
+//   setInterval(function () {
+//        $("#sliderContainer > .slide:first")
+//            .animate({right: "+=800"}, 2000)  
+//            .next()
+//            .css("right", mrgn)
+//            .fadeIn(0)
+//            .animate({right: "+=800"}, 2000)
+//            .end()
+//            
+//            .appendTo('#sliderContainer')
+//        $("#sliderContainer > .slide:last")
+//            .css("right", "-=800")
+//            .css("right", "-=800")
+//    }, 3000)
+//})
+    
+$(".next").click(function(){
+    $("#sliderContainer > .slide:first")
+        .animate({right: "+=800"}, 2000)
+        .next()
+        .css("right", mrgn)
+        .fadeIn(0)
+        .animate({right: "+=800"}, 2000)
+        .end()
+        .appendTo('#sliderContainer')
+    })
 })
